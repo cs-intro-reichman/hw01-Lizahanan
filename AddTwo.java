75a2847ea22771ccb7a5526818a2d2148b859b67
Lizahanan <154255907+Lizahanan@users.noreply.github.com>
2023-12-18 19:52:09 +0300

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -4,5 +4,8 @@
 public class AddTwo {
 	public static void main(String[] args) {
 		// Put your code here	
+            int a = 5;
+            int b = 8;
+         System.out.println( a + b );
 	}
 }
