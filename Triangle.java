c684c6a22b808fe6ed1677efbfe7b9495c0776de
Lizahanan <154255907+Lizahanan@users.noreply.github.com>
2023-12-20 00:58:47 +0300

    Triangle.java
    Final Assignment Elizaveta Khanan

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,18 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		int side1 = Integer.parseInt(args[0]);
+        int side2 = Integer.parseInt(args[1]);
+        int  side3 = Integer.parseInt(args[2]);
+
+        
+        
+            if ((side1 + side2 > side3) && (side2 + side3 > side1) && (side1 + side3 > side2)) {
+                System.out.println( side1 + " " + side2 + " " + side3 + " true ");
+            } else {
+                System.out.println(side1 + " " + side2 + " " + side3 + " false ");
+            }
+    
+        
 	}
 }
